7b380a3fedfd38c262e329e8841a270285a8329f
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-01 15:36:56 +0300

    googleauth model added because sessions don't work really good with drf

--- myApp/models.py
+++ myApp/models.py
@@ -4,11 +4,22 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Contact(models.Model):
-  name = models.CharField(max_length=200)
-  phone_number = PhoneNumberField(region='TR')
-  email = models.EmailField()
-  address = models.CharField(max_length=264)
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone_number = PhoneNumberField(region='TR')
+    email = models.EmailField()
+    address = models.CharField(max_length=264)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
   
-  def __str__(self):
-    return self.name
+  
+class GoogleAuth(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    token_uri = models.TextField()
+    client_id = models.TextField()
+    client_secret = models.TextField()
+    scopes = models.TextField()
+    expiry = models.DateTimeField()
